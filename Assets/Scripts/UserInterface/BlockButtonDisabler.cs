f74c72a9b830159b3f91089c72a00a455332c6d1
Dustin <dustinsurwill@gmail.com>
2019-10-01 23:25:21 -0700

    % forgot a file

--- Assets/Scripts/UserInterface/BlockButtonDisabler.cs
+++ Assets/Scripts/UserInterface/BlockButtonDisabler.cs
@@ -2,6 +2,7 @@
 using UnityEngine.UI;
 using UniVox;
 using UniVox.Launcher;
+using UniVox.Managers;
 
 public class BlockButtonDisabler : MonoBehaviour
 {
@@ -29,6 +30,14 @@ public class BlockButtonDisabler : MonoBehaviour
         last = btn;
         btn.interactable = false;
         physics.SetBlockId(id);
+    }
+
+    public void Register(BlockAsset block, Button btn)
+    {
+        if (GameManager.Registry.Blocks.TryGetIdentity(block.blockKey, out var blockIdentity))
+        {
+            btn.onClick.AddListener(() => HandleClick(blockIdentity.Block, btn));
+        }
     }
 
     // Update is called once per frame
