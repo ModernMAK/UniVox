84db523ef714796e26472465d15424a675eb6cfe
Marcus Kertesz <ModernMAK@Hotmail.com>
2019-11-16 21:04:05 -0800

    Moved Subgraphs to own folder

--- Assets/Scripts/UniVox/Launcher/AbstractBlock.cs
+++ Assets/Scripts/UniVox/Launcher/AbstractBlock.cs
@@ -3,10 +3,24 @@ using UniVox.Types;
 
 namespace UniVox.Launcher
 {
+    /// <summary>
+    /// Base Container for Blocks.
+    /// </summary>
+    /// See <see cref="NativeBlock"/> for the Native Variant.
     public abstract class AbstractBlock
     {
-
+        /// <summary>
+        /// Gets the Material Identity.
+        /// </summary>
+        /// <returns>This Block's Material Identity</returns>
+        // TODO Due to the nature of Block's we can't guarantee they are cached. Later we can choose to enforce it, or realize we can't enforce it.
         public abstract MaterialIdentity GetMaterial();
+
+        /// <summary>
+        /// Gets the Sub Material.
+        /// </summary>
+        /// <returns>This Block's SubMaterial</returns>
+        // TODO Due to the nature of Block's we can't guarantee they are cached. Later we can choose to enforce it, or realize we can't enforce it.
         public abstract FaceSubMaterial GetSubMaterial();
 
         public abstract Sprite GetBlockIcon();
@@ -18,6 +32,12 @@ namespace UniVox.Launcher
         }
     }
 
+    /// <summary>
+    /// This is the Native Block Type.
+    /// This struct cannot 
+    /// This is a struct-only, simplified version of it's sister-type: <seealso cref="AbstractBlock"/>
+    /// </summary>
+    /// See <see cref="AbstractBlock"/> for the Non-Native Variant.
     public struct NativeBlock
     {
         public NativeBlock(AbstractBlock blockRef)
